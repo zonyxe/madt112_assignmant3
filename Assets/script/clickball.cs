33ae9a7833cbdc8fc2e4effd505721bb86c1edd5
zonyxe <61337994+zonyxe@users.noreply.github.com>
2020-03-30 22:28:32 +0700

    Initialized git repository .

---
+++ Assets/script/clickball.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class clickball : MonoBehaviour
+{
+    public GameObject GoalGameObject;
+    private void OnMouseDrag()
+    {      
+       Vector3 mousePosWorld = Input.mousePosition;
+        mousePosWorld.z = transform.position.z - Camera.main.transform.position.z;
+        mousePosWorld = Camera.main.ScreenToWorldPoint(mousePosWorld);
+
+        transform.position = new Vector3(mousePosWorld.x, 
+                                            mousePosWorld.y, 
+                                            transform.position.z);
+       
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.Equals(GoalGameObject))
+        {
+            Debug.Log("win");
+        }
+    }
+}
